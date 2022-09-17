4df3221c6c995018ce0fb0a85a44d620e88c31b1
alantypoon <alantypoon@gmail.com>
2022-09-17 16:07:21 +0800

    commit

---
+++ a.darkmode.js
@@ -0,0 +1,113 @@
+// input: <div class="mode-switch"></div>
+//
+// taken from https://adminlte.io/docs/3.2/
+//
+
+document.ready = function (callback) {
+    // FF,Google
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', function () {
+            document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+            callback();
+        }, false)
+    }
+    // IE
+    else if (document.attachEvent) {
+        document.attachEvent('onreadystatechange', function () {
+              if (document.readyState == "complete") {
+                        document.detachEvent("onreadystatechange", arguments.callee);
+                        callback();
+               }
+        })
+    }
+    // others
+    else if (document.lastChild == document.body) {
+        callback();
+    }
+}
+
+// anonymous function
+document.ready(function(){
+
+    // create mode switch
+    var els = document.getElementsByClassName('mode-switch');
+    for (var el of els){
+        el.outerHTML = 
+            '<div class="theme-switch-wrapper">' +
+                '<label class="theme-switch" for="checkbox">' +
+                    '<input type="checkbox" id="checkbox">' +
+                    '<span class="slider round"></span>' +
+                '</label>' +
+            '</div>'
+        ;
+    }
+
+    var 
+        toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]'),
+        mainHeader = document.querySelector('.main-header'),
+        sideBar = document.querySelector('aside')
+        
+    ;
+    // os settings: https://stackoverflow.com/questions/56393880/how-do-i-detect-dark-mode-using-javascript
+    if (window.matchMedia){
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+            setDarkMode(1);
+        }    
+    } else if (localStorage.getItem('theme') === 'dark'){
+        // switch's previous settings
+        setDarkMode(1);
+    }
+
+    ///
+
+    function setDarkMode(dark){
+
+        console.log('setDarkMode', dark);
+        if (dark){
+
+            // set dark mode
+            if (!document.body.classList.contains('dark-mode')) {
+                document.body.classList.add("dark-mode");
+            }
+
+            if (mainHeader && mainHeader.classList.contains('navbar-light')) {
+                mainHeader.classList.add('navbar-dark');
+                mainHeader.classList.remove('navbar-light');
+            }
+            if (sideBar){
+                sideBar.className = sideBar.className.replace('-light', '-dark');
+            }
+            localStorage.setItem('theme', 'dark');
+            toggleSwitch.checked = true;
+
+        } else {
+        
+            // set light mode
+            if (document.body.classList.contains('dark-mode')) {
+                document.body.classList.remove("dark-mode");
+            }
+            if (mainHeader && mainHeader.classList.contains('navbar-dark')) {
+                mainHeader.classList.add('navbar-light');
+                mainHeader.classList.remove('navbar-dark');
+            }
+            if (sideBar){
+                sideBar.className = sideBar.className.replace('-dark', '-light');
+            }
+            localStorage.setItem('theme', 'light');
+            toggleSwitch.checked = false;
+
+        }
+    
+    }
+
+    // website switch event
+    toggleSwitch.addEventListener('change', function(e){
+        setDarkMode(e.target.checked?1:0);    
+    }, false);
+
+    // os switch event
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {        
+        setDarkMode(event.matches ? 1 : 0);
+    });    
+
+});
---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Dark Mode</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta id="viewport" name="viewport" content ="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+	<meta name="theme-color" content="#ffffff">
+	<noscript>Your browser does not support JavaScript!</noscript>
+	<head>
+	<title>Dark Mode</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta id="viewport" name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
+	<meta name="theme-color" content="#ffffff">
+	<noscript>Your browser does not support JavaScript!</noscript>
+
+	<script src="external/jquery-3.6.0.min.js"></script>
+	<link href="external/bootstrap.min.css" type="text/css" rel="stylesheet">
+	<script src="external/bootstrap.min.js"></script>	
+
+	<script src="a.darkmode.js"></script>
+	<link href="a.darkmode.css" type="text/css" rel="stylesheet">
+	<link href="a.darkmode-custom.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="mode-switch"></div>
+
+	<?php
+		include "bootstrap4-test-page.php";
+	?>
+
+</body>
+</html>
+
+<?php
+// $load_files = [
+// 	'external/jquery-3.6.0.min.js',
+// 	'external/bootstrap.min.css',
+// 	'external/bootstrap.min.js',
+// 	'a.darkmode.js',
+// 	'a.darkmode.css',
+// 	'a.darkmode-custom.css',
+// ];
+// include "../a.startup/index.php";
+?>
